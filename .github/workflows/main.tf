133f6c574ea8124eae3c9adfac61bac1f55dd0d3
seunaris <seunaaribilola@gmail.com>
2023-09-21 16:03:22 -0400

    mod

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -1,6 +1,9 @@
-#provider "aws" {}
+provider "aws" {}
 
-#resources "aws_vpc" "test" {
- #   cidr = "10.0.0.0/16"
-#}
+resources "aws_vpc" "test" {
+   cidr = "10.0.0.0/16"
 
+ tags = {
+    Name = "Actions-test"
+ }
+}
